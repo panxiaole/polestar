e29128dc561fa7cd4dcf8d4744dae514ce71abad
panxiaole <626840495@qq.com>
2019-05-07 11:09:35 +0800

    1.增加系统配置模块 2.增加spring boot admin监控

---
+++ polestar-biz/polestar-biz-sys-core/src/main/java/com/haier/polestar/biz/sys/BizSysApplication.java
@@ -0,0 +1,13 @@
+package com.haier.polestar.biz.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizSysApplication.class, args);
+	}
+
+}
