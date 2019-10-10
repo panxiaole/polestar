a269ed5dee6756b12e6b6e2828770e287fb0c69b
panxiaole <626840495@qq.com>
2019-10-10 17:44:35 +0800

    使用jasypt加密敏感词

---
+++ jasypt/jasypt.bat
@@ -0,0 +1,6 @@
+@echo off
+set/p input=«Î ‰»Îº”√‹◊÷∑˚¥Æ£∫
+set/p password=«Î ‰»Î√‹‘ø£∫
+echo º”√‹÷–...
+java -cp %USERPROFILE%\.m2\repository\org\jasypt\jasypt\1.9.3\jasypt-1.9.3.jar org.jasypt.intf.cli.JasyptPBEStringEncryptionCLI algorithm=PBEWithMD5AndDES input=%input% password=%password%
+pause
